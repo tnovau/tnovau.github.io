1c61982de9f649bac09c83a91161f04419157a8c
Tomás Novau Foresi <tnovau@gmail.com>
2025-11-17 19:51:07 +0100

    chore: updating page

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -7,9 +7,9 @@ module.exports = {
   githubUsername: 'tnovau',
   authorAvatar: '/images/avatar.jpg',
   authorDescription: `Sr. Full-Stack & DevOps - Javascript enthusiast, Guitarist and EU Citizen.<br />
-    In 2016 I started working as a TSQL developer.<br />
-    In 2017, I moved to JS and C#. Working on that languages since then. <br /><br />
-    I work as a Sr. Full-stack & DevOps mainly with <strong>AWS, CI/CD, Javascript, Typescript, C#, NodeJS & React</strong> at Tiendeo.`,
+    In 2016 I started working as a TSQL developer.<br />
+    In 2017, I moved to JS and C#. Working on that languages since then. <br /><br />
+    I work as a Sr. Full-stack & DevOps mainly with <strong>AWS, CI/CD, Javascript, Typescript, C#, NodeJS & React</strong> at Tiendeo.`,
   skills: [
     {
       name: 'Front-end',
@@ -43,12 +43,62 @@ module.exports = {
   jobs: [
     /* more jobs here */
     {
+      company: "Freelance (KLagan - Insurance client)",
+      begin: {
+        month: 'dec',
+        year: '2024'
+      },
+      duration: null,
+      occupation: "Sr. Full-stack developer & DevOps",
+      description: "Development of web component for claims and digital contracts modelling app in vue.js, node.js and AWS."
+    },
+    {
+      company: "Freelance (Tiendeo)",
+      begin: {
+        month: 'mar',
+        year: '2024'
+      },
+      duration: '6 mos',
+      occupation: "Sr. Full-stack developer & DevOps",
+      description: "Integration of APIs during acquisition of a company. Maintenance of infrastructure and CI/CD."
+    },
+    {
+      company: "Freelance (IT Systems - Logistic client)",
+      begin: {
+        month: 'nov',
+        year: '2023'
+      },
+      duration: '4 mos',
+      occupation: "Sr. Full-stack developer & DevOps",
+      description: "Maintenance of multiple internal tools for logistics management in React."
+    },
+    {
+      company: "Freelance (Tiendeo)",
+      begin: {
+        month: 'apr',
+        year: '2023'
+      },
+      duration: '6 mos',
+      occupation: "Sr. Full-stack developer & DevOps",
+      description: "Integration of APIs during acquisition of a company. Maintenance of infrastructure and CI/CD."
+    },
+    {
+      company: "Freelance (IT Systems - Retail client)",
+      begin: {
+        month: 'oct',
+        year: '2021'
+      },
+      duration: '1 yrs 5 mos',
+      occupation: "Sr. Full-stack developer & DevOps",
+      description: "Maintenance of multiple custom store-fronts for different e-commerce in React and Next.js (typescript)."
+    },
+    {
       company: "Tiendeo",
       begin: {
         month: 'nov',
         year: '2020'
       },
-      duration: null,
+      duration: '10 mos',
       occupation: "Sr. Full-stack developer",
       description: "Maintenance and development of a webapp to claim for money of your purchases, for multiple countries and currencies. Maintenance of infrastructure and CI/CD."
     },
